30ad6629824f5797d2b53163341e942cd43ff82b
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-02-03 22:34:29 +0530

    refactor: remove old authentication controller and service, and implement new auth routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,8 @@
 import { Hono } from "hono";
-import { getDb } from "@/db"; // Adjust the path as needed
 
-// Import types
-import { Env } from "@/types/common";
+// ** import routes
+import authApi  from "@/routes/auth";
 
-const route = new Hono<{ Bindings: Env }>();
+export const routes = new Hono();
 
-route.get("/", (c) => {
-  // c.env now includes DATABASE_URL, so this works as expected.
-  const db = getDb(c.env!.DATABASE_URL);
-  // You can now use `db` for your queries.
-  return c.text("Database initialized.");
-});
-
-export default route;
+routes.route("/auth-v2", authApi);
