3d5d7e7a0264e98433b57b0b0e8021acc3b67b85
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-02-04 18:40:43 +0000

    feat(edge-server): remove obsolete database schema and add @repo/db dependency

--- apps/edge-server/src/db/schema/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./tbl_image_hashes";
-export * from "./tbl_recycling_logs";
-export * from "./tbl_rewards";
-export * from "./tbl_user_fingerprint_logs";
-export * from "./tbl_users";
